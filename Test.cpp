04971a9fe20b28f9dbeea6f7579e0d3171db64b9
Zheng Li <zhenglichina@gmail.com>
2012-08-06 21:55:21 +0800

    t

--- Test.cpp
+++ Test.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 {
 	cout<<__cplusplus<<endl;
 	cout<<"hello world"<<endl;
-	string mytest=R"hhhhhhhhhh("hello\\\sdfsdf)()()()()()""""""""")hhhhhhhhhh";
+	string mytest=R"hhhhhhhhhh(hello\\\sdfsdf)()()()()()""""""""))hhhhhhhhhh";
 	cout<<mytest;
 	return 0;
 }
