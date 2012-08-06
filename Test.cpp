1ea2c9cbe65c5ce7412c30744420938c00e39b3d
Zheng Li <zhenglichina@gmail.com>
2012-08-06 20:02:50 +0800

    Test

--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(int argc, char const *argv[])
 {
 	cout<<__cplusplus<<endl;
 	cout<<"hello world"<<endl;
+	string mytest=R"hhhhhhhhhh("hello\\\sdfsdf)()()()()()""""""""")hhhhhhhhhh";
+	cout<<mytest;
 	return 0;
 }
